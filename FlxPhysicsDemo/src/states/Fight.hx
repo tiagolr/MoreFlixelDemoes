7ac494e24e8b8c1a253b03f9f2d18dc27f1698c9
proG4mr <prog4mr@gmail.com>
2013-07-22 22:51:51 +0100

    Graphics added to ragdoll.

--- FlxPhysicsDemo/src/states/Fight.hx
+++ FlxPhysicsDemo/src/states/Fight.hx
@@ -11,6 +11,9 @@ import nape.callbacks.InteractionCallback;
 import nape.callbacks.InteractionListener;
 import nape.callbacks.InteractionType;
 import flixel.addons.nape.FlxPhysSprite;
+import nape.callbacks.PreCallback;
+import nape.callbacks.PreFlag;
+import nape.callbacks.PreListener;
 import nape.constraint.PivotJoint;
 import nape.constraint.WeldJoint;
 import nape.dynamics.InteractionFilter;
@@ -32,7 +35,7 @@ class Fight extends FlxPhysState
 		
 		createWalls();
 		createSongoku();
-		//FlxPhysState.space.gravity.setxy(0, 100);
+		FlxPhysState.space.gravity.setxy(0, 100);
 		
 		
 		shooter = new Shooter();
@@ -40,6 +43,13 @@ class Fight extends FlxPhysState
 		
 		var songoku:Ragdoll = new Ragdoll(300,300);
 		songoku.init();
+		songoku.createGraphics("assets/GokuHead.png",
+								"assets/GokuUTorso.png",
+								"assets/GokuLTorso.png",
+								"assets/GokuUArm.png",
+								"assets/GokuLArm.png",
+								"assets/GokuULeg.png",
+								"assets/GokuLLeg.png");
 		add(songoku);
 		
 		FlxPhysState.space.listeners.add(new InteractionListener(CbEvent.BEGIN, 
@@ -62,6 +72,23 @@ class Fight extends FlxPhysState
 			shooter.getFirstAlive().kill();
 		}
 	}
+	
+	override public function update():Void 
+	{	
+		super.update();
+		
+		if (FlxG.keys.justPressed("G"))
+			disablePhysDebug(); // PhysState method to remove the debug graphics.
+			
+		if (FlxG.keys.justPressed("R"))
+			FlxG.resetState();
+			
+		if (FlxG.keys.justPressed("LEFT"))
+			FlxPhysicsDemo.prevState();
+		if (FlxG.keys.justPressed("RIGHT"))
+			FlxPhysicsDemo.nextState();
+	}
+	
 }
 
 
@@ -79,7 +106,6 @@ class Ragdoll extends FlxGroup
 	var lUArm:FlxPhysSprite; // left upper arm.
 	var lLArm:FlxPhysSprite; // left lower arm.
 	
-	var neck:FlxPhysSprite;
 	var head:FlxPhysSprite; // head.
 	
 	var lTorso:FlxPhysSprite; // lower torso.
@@ -94,9 +120,12 @@ class Ragdoll extends FlxGroup
 	var llegSize:FlxPoint;
 	var ulegSize:FlxPoint;
 	var uTorsoSize:FlxPoint;
-	var lTorsoSize:FlxPoint;
-	var neckSize:FlxPoint;
+	var lTorsoSize:FlxPoint; 
+	var neckHeight:Float;
 	var headRadius:Float;
+	
+	var limbOffset:Float;
+	var torsoOffset:Float;
 	
 	var startX:Float;
 	var startY:Float;
@@ -111,42 +140,57 @@ class Ragdoll extends FlxGroup
 		
 		Scale > 0 ? scale = Scale : scale = 1;
 		
-		larmSize = new FlxPoint(10, 30);
-		uarmSize = new FlxPoint(10, 30);
-		llegSize = new FlxPoint(10, 30);
-		ulegSize = new FlxPoint(10, 30);
-		uTorsoSize = new FlxPoint(25, 30);
-		lTorsoSize = new FlxPoint(25, 15);
-		neckSize = new FlxPoint(10, 20);
-		headRadius = 10;
+		//
+		larmSize = new FlxPoint(12 * scale, 45 * scale);
+		uarmSize = new FlxPoint(12 * scale, 40 * scale);
+		llegSize = new FlxPoint(15 * scale, 45 * scale);
+		ulegSize = new FlxPoint(15 * scale, 50 * scale);
+		uTorsoSize = new FlxPoint(35 * scale, 35 * scale);
+		lTorsoSize = new FlxPoint(35 * scale, 15 * scale);
+		neckHeight = 5 * scale;
+		headRadius = 15 * scale;
+		
+		limbOffset = 3 * scale;
+		torsoOffset = 5 * scale;
 		
 		startX = X;
 		startY = Y;
+		
 	}
 	
 	public function init()
 	{
 		sprites = new Array<FlxPhysSprite>();
 		
-		uTorso = new FlxPhysSprite(startX, startY); add(uTorso); sprites.push(uTorso); 
-		lTorso = new FlxPhysSprite(startX, startY + 30); add(lTorso); sprites.push(lTorso);
+		uTorso = new FlxPhysSprite(startX, startY); sprites.push(uTorso); 
+		lTorso = new FlxPhysSprite(startX, startY + 30); sprites.push(lTorso);
 		
-		rULeg = new FlxPhysSprite(startX + 20, startY + 100); add(rULeg); sprites.push(rULeg);
-		rLLeg = new FlxPhysSprite(startX + 20, startY + 150); add(rLLeg); sprites.push(rLLeg);
-		lULeg = new FlxPhysSprite(startX - 20, startY + 100); add(lULeg); sprites.push(lULeg);
-		lLLeg = new FlxPhysSprite(startX - 20, startY + 150); add(lLLeg); sprites.push(lLLeg);
+		rULeg = new FlxPhysSprite(startX + 20, startY + 100); sprites.push(rULeg);
+		rLLeg = new FlxPhysSprite(startX + 20, startY + 150); sprites.push(rLLeg);
+		lULeg = new FlxPhysSprite(startX - 20, startY + 100); sprites.push(lULeg);
+		lLLeg = new FlxPhysSprite(startX - 20, startY + 150); sprites.push(lLLeg);
 		
-		rUArm = new FlxPhysSprite(startX + 20, startY); add(rUArm); sprites.push(rUArm);
-		rLArm = new FlxPhysSprite(startX + 20, startY); add(rLArm); sprites.push(rLArm);
-		lUArm = new FlxPhysSprite(startX - 20, startY); add(lUArm); sprites.push(lUArm);
-		lLArm = new FlxPhysSprite(startX - 20, startY); add(lLArm); sprites.push(lLArm);
+		rUArm = new FlxPhysSprite(startX + 20, startY); sprites.push(rUArm);
+		rLArm = new FlxPhysSprite(startX + 20, startY); sprites.push(rLArm);
+		lUArm = new FlxPhysSprite(startX - 20, startY); sprites.push(lUArm);
+		lLArm = new FlxPhysSprite(startX - 20, startY); sprites.push(lLArm);
 		
-		neck = new FlxPhysSprite(startX, startY - 20); add(neck); sprites.push(neck);
-		head = new FlxPhysSprite(startX, startY - 40); add(head); sprites.push(head);
+		head = new FlxPhysSprite(startX, startY - 40); sprites.push(head);
 		
+		add(rLLeg);
+		add(lLLeg);
+		add(rULeg);
+		add(lULeg);
+		add(rLArm);
+		add(lLArm);
+		add(rUArm);
+		add(lUArm);
+		add(lTorso);
+		add(uTorso);
+		add(head);
 		
 		createBodies();
-		createFilterGroups();
+		createContactListeners();
 		createJoints();
 		
 		//setPos(startX, startY);
@@ -178,40 +222,89 @@ class Ragdoll extends FlxGroup
 		uTorso.createRectangularBody(uTorsoSize.x, uTorsoSize.y);
 		lTorso.createRectangularBody(lTorsoSize.x, lTorsoSize.y);
 		
-		neck.createRectangularBody(neckSize.x, neckSize.y);
 		head.createCircularBody(headRadius);
 	}
 	
-	function createFilterGroups()
+	function createContactListeners()
 	{
-		// group 11 1 - lower left leg ignores upper left leg 
-		// group 12 2- lower right leg ignores upper right leg
-		// group 21 4- upper left legs ignores lower torso 
-		// group 22 8- upper rigth legs ignores lower torso 
-		// group 3  16- lower torso ignores upper torso
-		// group 4  32- upper torso ignores upper arms 
-		// group 5  64- upper arms ignores lower arms | upper torso | lower torso
-		// group 6  128- lower arms
-		// group 7  256- neck ignores head | uppertorso
-		// group 8  512- head
+		// group 1 - lower left leg ignores upper left leg 
+		// group 2 - lower right leg ignores upper right leg
+		// group 3 - upper left legs ignores lower torso 
+		// group 4 - upper rigth legs ignores lower torso 
+		// group 5 - lower torso
+		// group 6 - upper torso ignores upper arms 
+		// group 7 - upper arms ignores lower arms | upper torso | lower torso
+		// group 8 - lower arms
 		
-		lLLeg.body.setShapeFilters(new InteractionFilter(1, ~4)); 
-		rLLeg.body.setShapeFilters(new InteractionFilter(2, ~8));
+		var group1:CbType = new CbType();
+		var group2:CbType = new CbType();
+		var group3:CbType = new CbType();
+		var group4:CbType = new CbType();
+		var group5:CbType = new CbType();
+		var group6:CbType = new CbType();
+		var group7:CbType = new CbType();
+		var group8:CbType = new CbType();
+		var group9:CbType = new CbType();
+		var group10:CbType = new CbType();
 		
-		lULeg.body.setShapeFilters(new InteractionFilter(4, ~16));
-		rULeg.body.setShapeFilters(new InteractionFilter(8, ~16));
+		lLLeg.body.cbTypes.add(group1);
+		rLLeg.body.cbTypes.add(group2);
+		lULeg.body.cbTypes.add(group3);
+		rULeg.body.cbTypes.add(group4);
+		lTorso.body.cbTypes.add(group5);
+		uTorso.body.cbTypes.add(group6);
+		lUArm.body.cbTypes.add(group7);
+		rUArm.body.cbTypes.add(group7);
+		lLArm.body.cbTypes.add(group8);
+		rLArm.body.cbTypes.add(group8);
+		head.body.cbTypes.add(group9);
 		
-		lTorso.body.setShapeFilters(new InteractionFilter(16, ~32));
-		uTorso.body.setShapeFilters(new InteractionFilter(32, ~64));
 		
-		lUArm.body.setShapeFilters(new InteractionFilter(64, ~128));
-		rUArm.body.setShapeFilters(new InteractionFilter(64, ~128));
+		var listener;
+		// lower left leg ignores upper left leg 
+		listener = new PreListener(InteractionType.COLLISION, group1, group3, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		// lower right leg ignores upper right leg
+		listener = new PreListener(InteractionType.COLLISION, group2, group4, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		// upper left legs ignores lower torso 
+		listener = new PreListener(InteractionType.COLLISION, group3, group5, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		// upper rigth legs ignores lower torso 
+		listener = new PreListener(InteractionType.COLLISION, group4, group5, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		// upper torso ignores upper arms 
+		listener = new PreListener(InteractionType.COLLISION, group6, group7, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		// upper arms ignores lower arms | upper torso | lower torso
+		listener = new PreListener(InteractionType.COLLISION, group7, group8, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		listener = new PreListener(InteractionType.COLLISION, group7, group6, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+		listener = new PreListener(InteractionType.COLLISION, group7, group5, ignoreCollision, 0, true);
+		listener.space = FlxPhysState.space;
+
+	}
+	
+	function ignoreCollision(cb:PreCallback):PreFlag {
+		return PreFlag.IGNORE;
+	}
+	
+	public function createGraphics(Head:String, UpperTorso:String, LowerTorso:String, UpperArm:String, LowerArm:String, UpperLeg:String, LowerLeg:String)
+	{
+		head.loadGraphic(Head);
+		uTorso.loadGraphic(UpperTorso);
+		lTorso.loadGraphic(LowerTorso);
 		
-		lLArm.body.setShapeFilters(new InteractionFilter(128));
-		rLArm.body.setShapeFilters(new InteractionFilter(128));
+		rULeg.loadGraphic(UpperLeg);
+		lULeg.loadGraphic(UpperLeg); lULeg.scale.x *= -1;
+		rLLeg.loadGraphic(LowerLeg);
+		lLLeg.loadGraphic(LowerLeg); lLLeg.scale.x *= -1;
 		
-		neck.body.setShapeFilters(new InteractionFilter(256, ~(512 | 32)));
-		head.body.setShapeFilters(new InteractionFilter(512));
+		rUArm.loadGraphic(UpperArm);
+		lUArm.loadGraphic(UpperArm); lUArm.scale.x *= -1;
+		rLArm.loadGraphic(LowerArm);
+		lLArm.loadGraphic(LowerArm); lLArm.scale.x *= -1;
 	}
 	
 	function createJoints() 
@@ -237,9 +330,9 @@ class Ragdoll extends FlxGroup
 		constrain = new PivotJoint(rULeg.body, lTorso.body, new Vec2(0, -ulegSize.y / 2 + 3), new Vec2(lTorsoSize.x / 2  - ulegSize.x / 2, lTorsoSize.y / 2 - 3));
 		constrain.space = FlxPhysState.space;
 		
-		// Upper torso with lower torso.
-		weldJoint = new WeldJoint(uTorso.body, lTorso.body, new Vec2(0, uTorsoSize.y / 2 - 3), new Vec2(0, -lTorsoSize.y / 2 - 3));
-		weldJoint.space = FlxPhysState.space;
+		// Upper torso with mid lower.
+		constrain = new PivotJoint(uTorso.body, lTorso.body, new Vec2(0, uTorsoSize.y / 2 + torsoOffset), new Vec2(0, -lTorsoSize.y / 2 - torsoOffset));
+		constrain.space = FlxPhysState.space;
 		
 		// Upper arms with Upper torso.
 		constrain = new PivotJoint(lUArm.body, uTorso.body, new Vec2(uarmSize.x / 2 - 3, -uarmSize.y / 2 + 3), new Vec2(-uTorsoSize.x / 2 + 3, -uTorsoSize.y / 2 + 3));
@@ -248,10 +341,8 @@ class Ragdoll extends FlxGroup
 		constrain.space = FlxPhysState.space;
 		
 		// Neck with upper torso.
-		weldJoint = new WeldJoint(neck.body, uTorso.body, new Vec2(0, neckSize.y / 4), new Vec2(0, -uTorsoSize.y / 2  + 3));
-		weldJoint.space = FlxPhysState.space;
-		// Neck with head.
-		constrain = new PivotJoint(neck.body, head.body, new Vec2(0, -neckSize.y / 4 ), new Vec2(0, headRadius));
+		constrain = new PivotJoint(uTorso.body, head.body, new Vec2(0, -uTorsoSize.y / 2 - neckHeight), new Vec2(0, headRadius));
 		constrain.space = FlxPhysState.space;
 	}
+
 }
